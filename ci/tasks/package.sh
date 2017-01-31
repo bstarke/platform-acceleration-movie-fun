7299c8424d188619cc963c3ae1c240a17baf1749
Brad Starkenberg <brad@starkenberg.net>
2017-01-31 16:58:40 -0700

    wrapper

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -4,7 +4,7 @@ set -ex
 
 pushd movie-fun-source
   echo "Packaging WAR"
-  ./graldew clean build -x test
+  ./gradlew clean build -x test
 popd
 
 war_count=`find movie-fun-source/target -type f -name *.war | wc -l`
